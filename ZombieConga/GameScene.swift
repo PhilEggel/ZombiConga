0e1db6f82ec5d1d140d4ae36465e078036afe020
PhilEagleDev <phileagledev@gmail.com>
2015-11-02 23:01:48 +0100

    creating the GameScene

--- ZombieConga/GameScene.swift
+++ ZombieConga/GameScene.swift
@@ -9,7 +9,18 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    let zombie = SKSpriteNode(imageNamed: "zombie1")
+    
     override func didMoveToView(view: SKView) {
         backgroundColor = SKColor.blackColor()
+        
+        let background = SKSpriteNode(imageNamed: "background1")
+        background.position = CGPoint(x: size.width / 2, y: size.height / 2)
+        background.zPosition = -1
+        addChild(background)
+        
+        zombie.position = CGPoint(x: 400, y: 400)
+        zombie.setScale(2.0)
+        addChild(zombie)
     }
 }
